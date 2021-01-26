5e128f4c69cf767db06a504a9e5a0ec1233eb82f
ml <marinoladis@hotmail.com>
2021-01-26 18:09:12 +0100

    added Navigation component dependency created needed Fragments

--- app/src/main/java/hr/ml/plavatvornicazadatak/view/MainActivity.java
+++ app/src/main/java/hr/ml/plavatvornicazadatak/view/MainActivity.java
@@ -1,8 +1,10 @@
-package hr.ml.plavatvornicazadatak;
+package hr.ml.plavatvornicazadatak.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import hr.ml.plavatvornicazadatak.R;
 
 public class MainActivity extends AppCompatActivity {
 
